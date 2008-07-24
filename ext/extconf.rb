350157aca0b1395a5a30229cfa5d824e79a3fb1e
Todd Fisher <todd.fisher@gmail.com>
2008-07-23 20:58:54 -0400

    check for curl-config otherwise fall back to having curl within a known path

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,11 +2,13 @@ require 'mkmf'
 
 dir_config('curl')
 
-unless have_library('curl') &&
-       have_header('curl/curl.h')
+if find_executable('curl-config')
+  $CFLAGS << " #{`curl-config --cflags`.strip}"
+  $LIBS << " #{`curl-config --libs`.strip}"
+elsif !have_library('curl') or !have_header('curl/curl.h')
   fail <<-EOM
   Can't find libcurl or curl/curl.h
-  
+
   Try passing --with-curl-dir or --with-curl-lib and --with-curl-include
   options to extconf.
   EOM
@@ -21,4 +23,3 @@ if try_compile('int main() { return 0; }','-Wall')
 end
 
 create_makefile('curb_core')
-
