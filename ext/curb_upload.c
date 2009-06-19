ad7fa23ea31343ea6b5fc727c57b4da1c5b3b8b2
Todd A. Fisher <taf2@web3.anerian.com>
2009-06-19 17:40:55 -0400

    support for big file uploads

---
+++ ext/curb_upload.c
@@ -0,0 +1,61 @@
+/* curb_upload.c - Curl upload handle
+ * Copyright (c)2009 Todd A Fisher. 
+ * Licensed under the Ruby License. See LICENSE for details.
+ */
+#include "curb_upload.h"
+extern VALUE mCurl;
+VALUE cCurlUpload;
+
+static void curl_upload_mark(ruby_curl_upload *rbcu) {
+  if (rbcu->stream) rb_gc_mark(rbcu->stream);
+}
+static void curl_upload_free(ruby_curl_upload *rbcu) {
+  free(rbcu);
+}
+
+VALUE ruby_curl_upload_new(VALUE klass) {
+  VALUE upload;
+  ruby_curl_upload *rbcu = ALLOC(ruby_curl_upload);
+  rbcu->stream = Qnil;
+  rbcu->offset = 0;
+  upload = Data_Wrap_Struct(klass, curl_upload_mark, curl_upload_free, rbcu);
+  return upload;
+}
+
+VALUE ruby_curl_upload_stream_set(VALUE self, VALUE stream) {
+  ruby_curl_upload *rbcu;
+  Data_Get_Struct(self, ruby_curl_upload, rbcu);
+  rbcu->stream = stream;
+  return stream;
+}
+VALUE ruby_curl_upload_stream_get(VALUE self) {
+  ruby_curl_upload *rbcu;
+  Data_Get_Struct(self, ruby_curl_upload, rbcu);
+  return rbcu->stream;
+}
+VALUE ruby_curl_upload_offset_set(VALUE self, VALUE offset) {
+  ruby_curl_upload *rbcu;
+  Data_Get_Struct(self, ruby_curl_upload, rbcu);
+  rbcu->offset = FIX2INT(offset);
+  return offset;
+}
+VALUE ruby_curl_upload_offset_get(VALUE self) {
+  ruby_curl_upload *rbcu;
+  Data_Get_Struct(self, ruby_curl_upload, rbcu);
+  return INT2FIX(rbcu->offset);
+}
+VALUE ruby_curl_upload_stream_size(VALUE self) {
+  ruby_curl_upload *rbcu;
+  Data_Get_Struct(self, ruby_curl_upload, rbcu);
+}
+
+/* =================== INIT LIB =====================*/
+void init_curb_upload() {
+  cCurlUpload = rb_define_class_under(mCurl, "Upload", rb_cObject);
+  rb_define_singleton_method(cCurlUpload, "new", ruby_curl_upload_new, 0);
+  rb_define_method(cCurlUpload, "stream=", ruby_curl_upload_stream_set, 1);
+  rb_define_method(cCurlUpload, "stream", ruby_curl_upload_stream_get, 0);
+  rb_define_method(cCurlUpload, "offset=", ruby_curl_upload_offset_set, 1);
+  rb_define_method(cCurlUpload, "offset", ruby_curl_upload_offset_get, 0);
+  rb_define_method(cCurlUpload, "size", ruby_curl_upload_stream_size, 0);
+}
---
+++ ext/curb_upload.h
@@ -0,0 +1,31 @@
+/* curb_upload.h - Curl upload handle
+ * Copyright (c)2009 Todd A Fisher. 
+ * Licensed under the Ruby License. See LICENSE for details.
+ */
+#ifndef __CURB_UPLOAD_H
+#define __CURB_UPLOAD_H
+
+#include "curb.h"
+
+#include <curl/easy.h>
+
+/*
+ * Maintain the state of an upload e.g. for putting large streams with very little memory
+ * out to a server. via PUT requests
+ */
+typedef struct {
+  VALUE stream;
+  size_t offset;
+} ruby_curl_upload;
+
+extern VALUE cCurlUpload;
+void init_curb_upload();
+
+VALUE ruby_curl_upload_new(VALUE klass);
+VALUE ruby_curl_upload_stream_set(VALUE self, VALUE stream);
+VALUE ruby_curl_upload_stream_get(VALUE self);
+VALUE ruby_curl_upload_offset_set(VALUE self, VALUE offset);
+VALUE ruby_curl_upload_offset_get(VALUE self);
+VALUE ruby_curl_upload_stream_size(VALUE self);
+
+#endif
