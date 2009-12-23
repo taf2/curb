2e9e6ef37e9d619a1c2313f485b26e98f8465f16
Todd A. Fisher <taf2@web3.anerian.com>
2009-12-23 09:19:44 -0500

    update gem version

--- ext/curb.h
+++ ext/curb.h
@@ -20,11 +20,11 @@
 #include "curb_macros.h"
 
 // These should be managed from the Rake 'release' task.
-#define CURB_VERSION   "0.6.1.0"
-#define CURB_VER_NUM   610
+#define CURB_VERSION   "0.6.2.0"
+#define CURB_VER_NUM   620
 #define CURB_VER_MAJ   0
 #define CURB_VER_MIN   6
-#define CURB_VER_MIC   1
+#define CURB_VER_MIC   2
 #define CURB_VER_PATCH 0
 
 
