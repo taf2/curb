11c2419c178e24a3c5503cef4d648e06adcf4125
Todd A. Fisher <taf2@web3.anerian.com>
2009-12-27 21:26:58 -0500

    update version

--- ext/curb.h
+++ ext/curb.h
@@ -20,11 +20,11 @@
 #include "curb_macros.h"
 
 // These should be managed from the Rake 'release' task.
-#define CURB_VERSION   "0.6.3.0"
-#define CURB_VER_NUM   630
+#define CURB_VERSION   "0.6.4.0"
+#define CURB_VER_NUM   640
 #define CURB_VER_MAJ   0
 #define CURB_VER_MIN   6
-#define CURB_VER_MIC   3
+#define CURB_VER_MIC   4
 #define CURB_VER_PATCH 0
 
 
