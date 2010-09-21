23c5390ee8cf3458bc8e7c9068237cbb3a059013
Florent Vaucelle <florent@silentale.com>
2010-09-21 18:18:05 +0200

    Added ability to pass an IO stream to Easy::Download. Added 1 new unit test.

--- tests/tc_curl_download.rb
+++ tests/tc_curl_download.rb
@@ -18,6 +18,36 @@ class TestCurbCurlDownload < Test::Unit::TestCase
     File.unlink(dl_path) if File.exist?(dl_path)
   end
 
+  def test_download_url_to_io
+    dl_url = "http://127.0.0.1:9129/ext/curb_easy.c"
+    dl_path = File.join(Dir::tmpdir, "dl_url_test.file")
+    reader, writer = IO.pipe
+
+    # Write to local file
+    fork do
+      begin
+        writer.close
+        File.open(dl_path, 'wb') { |file| file << reader.read }
+      ensure
+        reader.close rescue IOError # if the stream has already been closed
+      end
+    end
+
+    # Download remote source
+    begin
+      reader.close
+      curb = Curl::Easy.download(dl_url, writer)
+      Process.wait
+    ensure
+      writer.close rescue IOError # if the stream has already been closed, which occurs in Easy::download
+    end
+
+    assert File.exist?(dl_path)
+    assert_equal File.read(File.join(File.dirname(__FILE__), '..','ext','curb_easy.c')), File.read(dl_path)
+  ensure
+    File.unlink(dl_path) if File.exist?(dl_path)
+  end
+
   def test_download_bad_url_gives_404
     dl_url = "http://127.0.0.1:9129/this_file_does_not_exist.html"
     dl_path = File.join(Dir::tmpdir, "dl_url_test.file")
