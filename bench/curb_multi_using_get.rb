cdfe7738d456ae7546149816a05adf22b817dd7e
Todd Fisher <todd.fisher@gmail.com>
2011-03-20 16:05:58 -0400

    add benchmark for basic multi interfaces

---
+++ bench/curb_multi_using_get.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+$:.unshift File.expand_path(File.dirname(__FILE__))
+require '_usage'
+
+$:.unshift File.expand_path(File.join(File.dirname(__FILE__),'..','ext'))
+$:.unshift File.expand_path(File.join(File.dirname(__FILE__),'..','lib'))
+
+N = (ARGV.shift || 50).to_i
+BURL = 'http://127.0.0.1/zeros-2k'
+URLS = []
+N.times do
+  URLS << BURL
+end
+
+Memory.usage("Curl::Multi(#{N})") do
+  require 'curb'
+
+  Curl::Multi.get(URLS)
+end
