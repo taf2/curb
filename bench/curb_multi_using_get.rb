c6a763d7f8023bbfd7f396645a77b02a503e6510
Todd Fisher <todd.fisher@gmail.com>
2011-11-05 00:26:48 -0400

    clean up bench

--- bench/curb_multi_using_get.rb
+++ bench/curb_multi_using_get.rb
@@ -7,14 +7,23 @@ $:.unshift File.expand_path(File.join(File.dirname(__FILE__),'..','lib'))
 
 N = (ARGV.shift || 50).to_i
 BURL = 'http://127.0.0.1/zeros-2k'
-URLS = []
-N.times do|n|
-  URLS << BURL + "?n=#{n}"
-end
 
 Memory.usage("Curl::Multi.get(#{N})") do
   require 'curb'
 
-  Curl::Multi.get(URLS)
+  group = []
+  N.times do|n|
+    url = BURL + "?n=#{n}"
+
+    group << url
+
+    if group.size == 10
+      Curl::Multi.get(group)
+      group = []
+    end
+
+  end
+
+  Curl::Multi.get(group) if group.any?
 
 end
