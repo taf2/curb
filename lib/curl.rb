433edaaf2949c2f13a28e23636d28cb5e70d00f6
Todd Fisher <todd.fisher@gmail.com>
2013-06-05 10:50:56 -0400

    reset handle before each call, keeps dns resolver but clears settings

--- lib/curl.rb
+++ lib/curl.rb
@@ -8,6 +8,7 @@ module Curl
   
   def self.http(verb, url, post_body=nil, put_data=nil, &block)
     handle = Thread.current[:curb_curl] ||= Curl::Easy.new
+    handle.reset
     handle.url = url
     handle.post_body = post_body if post_body
     handle.put_data = put_data if put_data
