f1bcca300c57b17c9f33f11fc3069f43d93ebb69
Todd Fisher <todd.fisher@gmail.com>
2013-10-03 17:47:54 -0400

    release

--- ext/curb.h
+++ ext/curb.h
@@ -20,11 +20,11 @@
 #include "curb_macros.h"
 
 // These should be managed from the Rake 'release' task.
-#define CURB_VERSION   "0.8.4"
-#define CURB_VER_NUM   804
+#define CURB_VERSION   "0.8.5"
+#define CURB_VER_NUM   805
 #define CURB_VER_MAJ   0
 #define CURB_VER_MIN   8
-#define CURB_VER_MIC   4
+#define CURB_VER_MIC   5
 #define CURB_VER_PATCH 0
 
 
