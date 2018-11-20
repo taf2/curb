5b4e7b268c9c8419f2cf4631c0c34c6a6ab8da27
Todd Fisher <todd.fisher@gmail.com>
2018-11-20 11:52:42 -0500

    check for the nosignal and pump version

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -102,6 +102,9 @@ have_constant "curle_send_fail_rewind"
 have_constant "curle_ssl_engine_initfailed"
 have_constant "curle_login_denied"
 
+# older than 7.10.0
+have_constant "curlopt_nosignal"
+
 # older than 7.16.0
 have_constant "curlmopt_pipelining"
 
